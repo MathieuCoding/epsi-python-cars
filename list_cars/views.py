2ef270932b8ef10c7f34bee437ade546571f735d
MathieuCoding <polletmathieu.pro@gmail.com>
2023-12-06 11:24:52 +0100

    views added + list_cars template created

--- list_cars/views.py
+++ list_cars/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Car
 
-# Create your views here.
+def car_list(request):
+    cars = Car.objects.all()
+    return render(request, 'cars_list.html', {'cars': cars})
+
+def car_detail(request, pk):
+    car = Car.objects.get(pk=pk)
+    return render(request, 'car_detail.html', {'car': car})
