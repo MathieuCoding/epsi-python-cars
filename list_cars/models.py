1b66ec962c2541a767cfc1c8adbf8a96de9d2e52
MathieuCoding <polletmathieu.pro@gmail.com>
2023-12-06 12:30:05 +0100

    rating added in model

--- list_cars/models.py
+++ list_cars/models.py
@@ -7,5 +7,6 @@ class Car(models.Model):
     year = models.IntegerField()
     price = models.IntegerField()
     engine = models.CharField(max_length=200)
+    rating = models.IntegerField(null=True, blank=True)
     def __str__(self):
         return self.brand + ' - ' + self.model + ' - ' + self.engine
