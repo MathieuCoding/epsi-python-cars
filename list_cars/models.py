8337e41ebb89d2648ba30c69c65269b2303306f6
MathieuCoding <polletmathieu.pro@gmail.com>
2023-12-06 10:44:01 +0100

    model added

--- list_cars/models.py
+++ list_cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    brand = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    year = models.IntegerField()
+    price = models.IntegerField()
+    engine = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name + ' - ' + self.brand + ' - ' + self.model
